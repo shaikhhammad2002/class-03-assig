42de24a3933d33cc5b3688f3e90ddd07cda45449
shaikhhammad2002 <shaikhhammad2002@gmail.com>
2024-11-01 21:20:56 +0500

    Class 03 Assignment

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center">Muhammad Hammad Shaikh</h1>
+    </div>
+  );
+};
+
+export default HomePage;
